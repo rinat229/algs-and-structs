b71d026178c031a34c650e79dac57b40e96b941f
rinat229 <rdkhaliulin@miem.hse.ru>
2022-05-15 18:39:31 +0300

    updated treap

--- treap_by_implicit_key.cpp
+++ treap_by_implicit_key.cpp
@@ -4,20 +4,32 @@
 #include <cstdlib>
 #include <ctime>
 
+int operation(int a, int b, int c){
+    return std::min(std::min(a, b), c);
+}
+
 struct TreapNode{
+    static int const neutral_element = INT32_MAX;
     int value;
     int priority;
-    int size = 1000;
+    int size = 1;
+    int query_ans;
     std::unique_ptr<TreapNode> left_node;
     std::unique_ptr<TreapNode> right_node;
 
     TreapNode(int value, int priority) : value(value), priority(priority),
-    left_node(nullptr), right_node(nullptr) {}
+    query_ans(value), left_node(nullptr), right_node(nullptr){}
     TreapNode() : left_node(nullptr), right_node(nullptr) {}
     void update(){
         size = l_size() + r_size() + 1;
+        query_update();
     }
 
+    void query_update(){
+        int left_val = left_node ? left_node->query_ans : neutral_element;
+        int right_val = right_node ? right_node->query_ans : neutral_element;
+        query_ans = operation(left_val, value, right_val);
+    }
     int l_size(){
         return left_node == nullptr ? 0 : left_node->size;
     }
@@ -38,14 +50,18 @@ int size(node_type& __node){
 class Treap{
     std::pair<node_type, node_type> split(node_type &treap_node, int count);
     node_type merge(node_type &treap_node1, node_type &treap_node2);
-
+    
     public:
+        std::vector<int> order;
         void update(int position);
         node_type root;
         void insert(int value, int priority, int position);
         void remove(int key);
-        void print_values(node_type &node);
+        void preorder(node_type &node, bool print = true);
+        void preorder();
+        void print_values();
         void move_to_front(int l_i, int r_i);
+        int query(int l_i, int r_i);
         Treap() : root(nullptr){}
 };
 
@@ -70,7 +86,6 @@ std::pair<node_type, node_type> Treap::split(node_type &treap_node, int count){
     }
 }
 
-
 node_type Treap::merge(node_type &treap_node1, node_type &treap_node2){
     if(treap_node2 == nullptr){
         return std::move(treap_node1);
@@ -93,14 +108,13 @@ node_type Treap::merge(node_type &treap_node1, node_type &treap_node2){
     }
 }
 
-
 void Treap::insert(int value, int priority, int position){
     auto __pair = split(root, position);
     auto new_treap_node = std::make_unique<TreapNode>(value, priority);
 
     auto T1 = merge(__pair.first, new_treap_node);
-    auto T2 = merge(T1, __pair.second);
-    root = std::move(T2);
+    root = merge(T1, __pair.second);
+    // root = T2;
 }
 
 
@@ -113,35 +127,60 @@ void Treap::move_to_front(int l_i, int r_i){
     root = std::move(T2);
 }
 
+int Treap::query(int l_i, int r_i){
+    auto __pair1 = split(root, r_i);
+    auto __pair2 = split(__pair1.first, l_i);
+
+    int result = __pair2.second->query_ans;
+    auto T1 = merge(__pair2.first, __pair2.second);
+    root = merge(T1, __pair1.second);
+
+    return result;
+}
 
 void Treap::update(int position){
     auto __pair = split(root, position);
-    root = std::move(merge(__pair.first, __pair.second));
+    root = merge(__pair.first, __pair.second);
 }
 
-void Treap::print_values(node_type &node){
+void Treap::preorder(){
+    order.clear();
+    preorder(root, false);
+}
+
+void Treap::print_values(){
+    preorder(root, true);
+}
+void Treap::preorder(node_type &node, bool print){
     if(node != nullptr){
-        print_values((*node).left_node);
-        std::cout << (*node).value << ' ';
-        print_values((*node).right_node);
+        preorder(node->left_node);
+        if (print) {
+            std::cout << node->value << ' '; //<< node->size << '\t';
+        }
+        else{
+            order.push_back(node->value);
+        }
+        preorder(node->right_node);
     }
 }
 
 
 int main(){
+    std::ios::sync_with_stdio(false); std::cin.tie(0);
     Treap treap;
     std::srand(std::time(nullptr));
-    int n, m;
-    std::cin >> n >> m;
+    int n;
+    std::cin >> n;
 
-    for(int idx = 1; idx <= n; idx++){
-        treap.insert(idx, rand() % 100000, idx);
+    for(int idx = 0; idx < n; idx++){
+        char type;
+        int l, r;
+        std::cin >> type >> l >> r;
+        if(type == '+'){
+            treap.insert(r, rand() % 100000, l);
+        }
+        else if(type == '?'){
+            std::cout << treap.query(l - 1, r) << '\n';
+        }
     }
-
-    for(int i = 0; i < m; i++){
-        int l_i, r_i;
-        std::cin >> l_i >> r_i;
-        treap.move_to_front(l_i - 1, r_i);
-    }
-    treap.print_values(treap.root);
 }
